b6535a721135b5dfe74a29195508337f0b42d935
roman <arrumanov@mail.ru>
2017-03-31 22:00:40 +0300

    first commit

---
+++ Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Lab1WebApp.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="StyleSheet1.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="display: inline-block">
+    
+        Выбор темы:<br />
+        <asp:RadioButtonList ID="RadioButtonList1" runat="server">
+            <asp:ListItem>Theme1</asp:ListItem>
+            <asp:ListItem>Theme2</asp:ListItem>
+        </asp:RadioButtonList>
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Выбрать" />
+        <br />
+        <br />
+        <br />
+    
+        Введите логин:&nbsp;&nbsp;
+        <asp:TextBox ID="Login" runat="server" CssClass="text" Width="150px"></asp:TextBox>
+&nbsp;<asp:Label ID="ValidLogin" runat="server"></asp:Label>
+        <br />
+        <br />
+        Введите пароль:
+        <asp:TextBox ID="Password" runat="server" CssClass="text" TextMode="Password" Width="151px"></asp:TextBox>
+&nbsp;<asp:Label ID="ValidPassword" runat="server"></asp:Label>
+        <br />
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Submit" runat="server" Text="Submit" OnClick="Submit_Click" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace Lab1WebApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            if (Session["Theme"] == null)
+            {
+                // Тема не выбрана
+                Page.Theme = "";
+            }
+            else
+            {
+                Page.Theme = (string)Session["Theme"];
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                // Заполнить список доступными темами, прочитав папки внутри App_Themes
+                DirectoryInfo themeDir = new DirectoryInfo(Server.MapPath("App_Themes"));
+                RadioButtonList1.DataTextField = "Name";
+                RadioButtonList1.DataSource = themeDir.GetDirectories();
+                RadioButtonList1.DataBind();
+            }
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            bool result = true;
+
+            if (Login.Text != "admin") { ValidLogin.Text = "Неверный логин"; result = false; }
+            else { ValidLogin.Text = ""; }
+            if (Password.Text != "123") { ValidPassword.Text = "Неверный пароль"; result = false; }
+            else { ValidPassword.Text = ""; }
+
+            if (result) { ValidLogin.Text = "Вы"; ValidPassword.Text = "авторизованы"; }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // Установить выбранную тему
+            Session["Theme"] = RadioButtonList1.SelectedValue;
+
+            // Обновить страницу
+            Server.Transfer(Request.FilePath);
+        }
+    }
+}
